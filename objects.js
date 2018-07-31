2ded1c8c5ef55411b0222aa8966fdad77f13b0d8
skylerwphillipstx@gmail.com Phillips <skylerwphillipstx@gmail.com>
2018-07-31 02:03:44 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(play, artist, song) {
   return playlist;
 }
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
-  return playlist
+  delete playlist.artistName;
+  playlist;
 }
